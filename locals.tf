6a05f33a819dd685a211aa5733bfc48524996594
chinchalinchin <chinchalinchin@gmail.com>
2025-02-04 10:42:50 -0500

    add automation and policies

--- locals.tf
+++ locals.tf
@@ -4,6 +4,14 @@ locals {
             for f in fileset("${path.module}/documents/commands", "*.yaml"):
                 split(".", f)[0]    => file("${path.module}/documents/commands/${f}")
         }
+        automation                  = {
+            for f in fileset("${path.module}/documents/automation", "*.yaml"):
+                split(".", f)[0]    => file("${path.module}/documents/automation/${f}")
+        }
+        policy                      = {
+            for f in fileset("${path.module}/documents/policy", "*.yaml"):
+                split(".", f)[0]    => file("${path.module}/documents/policies/${f}")
+        }
     }
 
     name                            = upper(join("-", [module.platform.prefix,
