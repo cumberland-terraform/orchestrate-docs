c183d747c664588663996612add48b5ae71016c1
chinchalinchin <chinchalinchin@gmail.com>
2025-02-04 14:19:02 -0500

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "documents" {
     description                 = "SSM Document Metadata"
     value                       = {
-        automation              = aws_ssm_document.Automation
+        automation              = aws_ssm_document.automation
         commands                = aws_ssm_document.commands
         policies                = aws_ssm_document.policies
     }
