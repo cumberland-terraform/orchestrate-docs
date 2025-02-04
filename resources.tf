7b38c0e4b45ded50ea5c92a507a86d380bb9b464
chinchalinchin <chinchalinchin@gmail.com>
2025-02-04 14:39:33 -0500

    ignore tag calculations

--- resources.tf
+++ resources.tf
@@ -1,6 +1,10 @@
 resource "aws_ssm_document" "commands" {
     for_each                = local.documents.commands
     
+    lifecycle {
+        ignore_changes      = [ tags ]
+    }
+
     document_format         = "YAML"
     document_type           = "Command"
     content                 = each.value
@@ -10,6 +14,10 @@ resource "aws_ssm_document" "commands" {
 
 resource "aws_ssm_document" "automation" {
     for_each                = local.documents.automation
+
+    lifecycle {
+        ignore_changes      = [ tags ]
+    }
 
     document_format         = "YAML"
     document_type           = "Automation"
@@ -21,6 +29,10 @@ resource "aws_ssm_document" "automation" {
 resource "aws_ssm_document" "policies" {
     for_each                = local.documents.policies
 
+    lifecycle {
+        ignore_changes      = [ tags ]
+    }
+    
     document_format         = "YAML"
     document_type           = "Policy"
     content                 = each.value
