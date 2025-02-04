f6c6670ab9247493f0bbbb0f6cb43bff3e4e3698
chinchalinchin <chinchalinchin@gmail.com>
2025-02-04 14:24:21 -0500

    encode with yaml

--- resources.tf
+++ resources.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_document" "commands" {
     for_each                = local.documents.commands
     
     document_type           = "Command"
-    content                 = each.value
+    content                 = yamlencode(each.value)
     name                    = upper(join("-", [local.name, each.key]))
     tags                    = local.tags
 }
@@ -11,7 +11,7 @@ resource "aws_ssm_document" "automation" {
     for_each                = local.documents.automation
 
     document_type           = "Automation"
-    content                 = each.value
+    content                 = yamlencode(each.value)
     name                    = upper(join("-", [local.name, each.key]))
     tags                    = local.tags
 }
@@ -20,7 +20,7 @@ resource "aws_ssm_document" "policies" {
     for_each                = local.documents.automation
 
     document_type           = "Policy"
-    content                 = each.value
+    content                 = yamlencode(each.value)
     name                    = upper(join("-", [local.name, each.key]))
     tags                    = local.tags
 }
