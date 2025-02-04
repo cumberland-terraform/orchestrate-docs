a67538bbc35b439399d7a746f47d1d64568da980
chinchalinchin <chinchalinchin@gmail.com>
2025-02-04 10:44:48 -0500

    add policy and automation resources

--- resources.tf
+++ resources.tf
@@ -1,6 +1,25 @@
 resource "aws_ssm_document" "commands" {
     for_each                = local.documents.commands
+    
     document_type           = "Command"
+    content                 = each.value
+    name                    = join("-", [local.name, each.key])
+    tags                    = local.tags
+}
+
+resource "aws_ssm_document" "automation" {
+    for_each                = local.documents.automation
+
+    document_type           = "Automation"
+    content                 = each.value
+    name                    = join("-", [local.name, each.key])
+    tags                    = local.tags
+}
+
+resource "aws_ssm_document" "policy" {
+    for_each                = local.documents.automation
+
+    document_type           = "Policy"
     content                 = each.value
     name                    = join("-", [local.name, each.key])
     tags                    = local.tags
